3597a33cc53c8d7a52c5724d646dd9fc03d1f63e
screfy <screfy@pm.me>
2021-03-21 20:03:56 +0100

    added Error4xxTemplate.php

---
+++ app/Module/Front/Error4xx/Error4xxTemplate.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Screfy\Module\Front\Error4xx;
+
+use Screfy\Module\BaseTemplate;
+
+class Error4xxTemplate extends BaseTemplate
+{
+    public int $httpCode;
+}
