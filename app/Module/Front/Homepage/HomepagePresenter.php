be3f53fda568d7a79921846808c74027c3e75ef8
screfy <screfy@pm.me>
2021-03-21 19:40:18 +0100

    added HomepagePresenter.php

---
+++ app/Module/Front/Homepage/HomepagePresenter.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Screfy\Module\Front\Homepage;
+
+use Screfy\Module\Front\BaseFrontPresenter;
+
+final class HomepagePresenter extends BaseFrontPresenter
+{
+}
