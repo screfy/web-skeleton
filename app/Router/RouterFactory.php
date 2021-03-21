0ab21e5da6100961f71c355d416124025cb095a2
screfy <screfy@pm.me>
2021-03-21 19:34:37 +0100

    added RouterFactory.php

---
+++ app/Router/RouterFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Screfy\Router;
+
+use Nette\Application\Routers\RouteList;
+use Nette\StaticClass;
+
+class RouterFactory
+{
+    use StaticClass;
+
+    public static function create(): RouteList
+    {
+        $router = new RouteList();
+        $router
+            ->withModule('Front')
+            ->addRoute('<presenter>[/<action>]', 'Homepage:default')
+            ->end();
+        return $router;
+    }
+}
